88a23ce78d3b5d67b5b92c02c95e18d8ac24e7eb
polearm123 <shaggywaggy29@gmail.com>
2021-09-19 20:11:57 +0300

    completed part 1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const Header =  (props) => {
+  return(
+    <div>
+      <h1>{props.course}</h1>
     </div>
-  );
+
+  )
+}
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.part} : {props.exercise}</p>
+    </div>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
+    </div>
+  )
 }
 
-export default App;
+const Total = (props) => {
+  return (
+    <div>
+      <p>total number of exercises is {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
+
+    </div>
+  )
+
+}
+const App = () => {
+
+  const [counter,setCounter] = useState(0)
+
+  const handleClick = () => {
+    console.log("clicked")
+    console.log(`counter : ${counter}`)
+    setCounter(counter+1)
+  }
+  const resetClicks = () => {
+    setCounter(0)
+  }
+  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts}/>
+      <button onClick={handleClick}>
+        plus : {counter}
+      </button>
+      <button onClick={resetClicks}>
+        reset
+      </button>
+
+    </div>
+  )
+}
+
+export default App
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Button = (props) => {
+  return(
+    <div>
+      <button onClick={props.onClick}>
+        {props.text}
+      </button>
     </div>
-  );
+  )
+}
+const StatisticLine = (props) => {
+
+  return(
+
+    <tr>
+
+      <td>
+        {props.text} 
+      </td>
+
+      <td>
+        {props.value}
+      </td>
+
+    </tr>
+     
+  )
+}
+const Statistics = (props) => {
+
+  const totalVotes = props.good+props.bad+props.neutral
+  const positive = props.good/totalVotes
+  const average = ((props.good+((-1)*props.bad))/totalVotes)*100
+  const positiveString = String(positive*100) + " %"
+  
+  if(totalVotes>0){
+    return(
+      <div>
+        <table>
+          <tbody>
+            
+              <StatisticLine text="good" value={props.good}/>
+              <StatisticLine text="neutral" value={props.neutral}/>
+              <StatisticLine text="bad" value={props.bad}/>
+              <StatisticLine text="all" value={totalVotes}/>  
+              <StatisticLine text="average" value={average}/>
+              <StatisticLine text="positive" value={positiveString}/>
+            
+          </tbody>
+        </table>
+      </div>
+    )}
+
+  return(
+    <div>
+      no feedback given
+    </div>
+
+  )
 }
 
-export default App;
+const App = () => {
+
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+
+  const goodInc = () => {
+    setGood(good+1)
+  }
+  const neutralInc = () => {
+    setNeutral(neutral+1)
+  }
+  const badInc = ()=> {
+    setBad(bad+1)
+  }
+
+  //confitional render based on totalVotes
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={goodInc} text='good' />
+      <Button onClick={neutralInc} text='neutral' />
+      <Button onClick={badInc} text='bad' />
+
+      <h1>statistics</h1>
+
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+      
+      
+    </div>
+  )
+}
+
+export default App
